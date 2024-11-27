8970f2bd073829c06d580c6d3c22a20d432009f8
Sitrus1 <sirojiddin2010all@gmail.com>
2024-11-27 18:47:12 +0500

    ready

---
+++ assets/about-DVHkGdkJ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-fyPGCoNP.js";const r=()=>t.jsx("div",{children:"about"});export{r as default};
---
+++ assets/not-found-Dr3_NRMJ.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-fyPGCoNP.js";const n=()=>o.jsx("div",{children:"not found"});export{n as default};
